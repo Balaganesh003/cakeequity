2714a39419239facc0bb8b448b09e396e416de5e
Balaganesh K <k.balaganesh26@gmail.com>
2023-08-01 23:03:47 +0530

    added the assets and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'CabinetGrotesk-Variable': ['CabinetGrotesk-Variable', 'sans-serif'],
+        'CabinetGrotesk-Thin': ['CabinetGrotesk-Thin', 'sans-serif'],
+        'CabinetGrotesk-Extralight': [
+          'CabinetGrotesk-Extralight',
+          'sans-serif',
+        ],
+        'CabinetGrotesk-Light': ['CabinetGrotesk-Light', 'sans-serif'],
+        'CabinetGrotesk-Regular': ['CabinetGrotesk-Regular', 'sans-serif'],
+        'CabinetGrotesk-Medium': ['CabinetGrotesk-Medium', 'sans-serif'],
+        'CabinetGrotesk-Bold': ['CabinetGrotesk-Bold', 'sans-serif'],
+        'CabinetGrotesk-Extrabold': ['CabinetGrotesk-Extrabold', 'sans-serif'],
+        'CabinetGrotesk-Black': ['CabinetGrotesk-Black', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
